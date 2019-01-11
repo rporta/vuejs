91f96be6f6e0d4d2c0735611f7da596f94e7e2a1
Ramiro Portas <rporta@teceng-gaming.com.ar>
2019-01-11 11:30:09 -0300

    se modifica estructura

--- js/components/app.js
+++ js/components/app.js
@@ -1,14 +1,13 @@
 var app = new Vue({
 	el: '#app',
 	data: {
-		'blogTitle' : "algo",
 		'color' : colors,
 		'colorHexa' : colorsHexa,
 		'colorText' : colorsText,
 		'colorFull' : colorsFull
 	},
 	components: {
-		'Indeterminate' : progressIndeterminate
+		[progress.name] : progress
 	},
 	methods:{
 		create : function (element){
@@ -16,6 +15,15 @@ var app = new Vue({
 		},
 		newComponent : function(component){
 			return eval("new this.$options.components." + component + "()");
+		},
+		generateId: function(length){
+			var id = "";
+			var char_list = 
+			"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+			for(var i=0; i < length; i++ ){  
+				id += char_list.charAt(Math.floor(Math.random() * char_list.length));
+			}
+			return '-' + id;
 		}
 	}
 });
--- js/components/components.js
+++ js/components/components.js
@@ -1,19 +1,17 @@
-var progressIndeterminate = {
+var progress = {
+	name : "Indeterminate",
 	data: function () {
 		return {
 			color : new Array("", "")
 		}
 	},
-	template: '<div v-bind:id="$options._componentTag + this.generateId(5)"  v-bind:class="color[0]"  class=" progress " >{{generateLog(this)}}<div v-bind:class="color[1]" class="indeterminate"></div></div>',
+	template: '<div v-bind:id="this.generateId(5)"  v-bind:class="color[0]"  class=" progress " ><div v-bind:class="color[1]" class="indeterminate"></div></div>',
 	methods: {
-		generateLog : function(log){
-			console.log(log);
-		},
 		insertDom: function(ref){
 			return this.$mount(ref).$el;
 		},
 		generateId : function (arg) {
-			return generateId(arg);	
+			return this.$options.name + app.generateId(arg);	
 		},
 		create : function (element){
 			return this.$el.append(element);
