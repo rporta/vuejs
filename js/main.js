3bc58fb322bbd088fdbac0b697bc44e0ba35a111
rporta <rporta@gmail.com>
2019-01-20 15:29:13 -0300

    se actualiza libreria a version mas estable

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 //aca trabajo con la instancia
 //example:
 app.setColor(app.generateColor());
-var preloaderFull = app.newComponent('c-preloaderFull').setSectionColor(app.generateColor());
+var preloaderFull = app.newComponent('c-preloaderCircleFull').setSectionColor(app.generateColor()).setColorHexa(app.generateColorHexa());
 
 setTimeout(() => {
 	preloaderFull.setShow(false);
@@ -9,9 +9,9 @@ setTimeout(() => {
 
 app.create(preloaderFull);
 
-var header = app.newComponent('c-header').setText("soy header").setColor(app.generateColor()).setColorText(app.generateColorText()).setTextAling(app.textAling.c);
-var main = app.newComponent('c-main').setText("soy main").setColor(app.generateColor()).setColorText(app.generateColorText()).setTextAling(app.textAling.c);
 var footer = app.newComponent('c-footer').setText("soy footer").setColor(app.generateColor()).setColorText(app.generateColorText()).setTextAling(app.textAling.c);
+var main = app.newComponent('c-main').setText("soy main").setColor(app.generateColor()).setColorText(app.generateColorText()).setTextAling(app.textAling.c);
+var header = app.newComponent('c-header').setText("soy header").setColor(app.generateColor()).setColorText(app.generateColorText()).setTextAling(app.textAling.c);
 
 app.create(header);
 app.create(main);
